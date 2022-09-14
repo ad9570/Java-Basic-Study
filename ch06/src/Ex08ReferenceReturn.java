faed04aeefc9190f4df9de0793c9e7414c33d7a5
ad9570 <loufs_@naver.com>
2022-09-14 10:11:03 +0700

    ch6-3 mod

--- ch06/src/Ex08ReferenceReturn.java
+++ ch06/src/Ex08ReferenceReturn.java
@@ -12,7 +12,7 @@ class Ex08ReferenceReturn {	// 빈 스택
 								// copy()에서 생성된 저장공간이 main()에서 사용 가능하게 됨
 		System.out.println("d1.x = "+d1.x);	// d1.x와 d2.x는 다른 저장공간을 사용하지만,
 		System.out.println("d2.x = "+d2.x);	// copy()에서 x값을 복사했기 때문에 d1.x값과 d2.x값이 똑같다.
-	}
+	}	// 빈 스택
 
 	static Data3 copy(Data3 d) {	// copy()의 지역변수 d - d1의 주소값이 복사됨
 		Data3 tmp = new Data3();    // 새로운 객체 tmp를 생성한다. - 새로운 저장 공간
