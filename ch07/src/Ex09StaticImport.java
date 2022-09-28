0785654216aad5314d8328a18444c37f599f29ba
ad9570 <loufs_@naver.com>
2022-09-28 12:21:33 +0700

    staticimport

---
+++ ch07/src/Ex09StaticImport.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;	// System 클래스의 클래스 변수 out
+import static java.lang.Math.*;	// Math 클래스의 모든 멤버
+
+class Ex09StaticImport {
+	public static void main(String[] args) {	
+		// System.out.println(Math.random());
+		out.println(random());	// random() : Math 클래스의 클래스 메서드
+
+		// System.out.println("Math.PI :"+Math.PI);
+		out.println("Math.PI :" + PI);	// PI : Math 클래스의 클래스 상수
+	}
+}
--- ch07/src/com/codechobo/book/PackageTest.java
+++ ch07/src/com/codechobo/book/PackageTest.java
@@ -1,6 +1,6 @@
-package com.codechobo.book;
+package com.codechobo.book;	// 패키지 선언
 
-public class PackageTest {
+public class PackageTest {	// com.codechobo.book.PackageTest
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
