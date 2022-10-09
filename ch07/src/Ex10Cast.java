c63af71710c1df01155bb0d7eae2a51229c1a90f
ad9570 <loufs_@naver.com>
2022-10-09 22:10:33 +0700

    cast, instanceof

---
+++ ch07/src/Ex10Cast.java
@@ -0,0 +1,44 @@
+class Ex10Cast {
+	public static void main(String args[]) {
+		Car car = null;
+		FireEngine fe = new FireEngine();
+		FireEngine fe2 = null;
+//		Ambulance am = new Ambulance();
+
+		fe.water();
+		
+		car = fe;    // 조상타입 ← 자손타입. car = (Car)fe;에서 형변환이 생략됨
+//		car.water();	// ERROR, 조상타입 참조변수로 자손타입만 가진 멤버 사용 불가능
+		car.drive();
+		
+		fe2 = (FireEngine)car; // 자손타입 ← 조상타입. 형변환 생략 불가
+		fe2.water();
+		
+//		fe = (FireEngine)am;	// ERROR, 조상-자손 관계가 아닌 클래스 타입 간 형변환 불가
+	}
+}
+
+class Car {
+	String color;
+	int door;
+
+	void drive() { 	// 운전하는 기능
+		System.out.println("drive, Brrrr~");
+	}
+
+	void stop() {  	// 멈추는 기능	
+		System.out.println("stop!!!");	
+	}
+}
+
+class FireEngine extends Car {	// 소방차
+	void water() {	// 물을 뿌리는 기능
+		System.out.println("water!!!");
+	}
+}
+
+class Ambulance extends Car {
+	void siren() {
+		System.out.println("zzz");
+	}
+}
---
+++ ch07/src/Ex11CastError.java
@@ -0,0 +1,17 @@
+class Ex11CastError {
+	public static void main(String[] args) {
+		Car c = null;			// 형변환이 가능한지 여부보다 실제 인스턴스가 적절한지가 중요함
+		FireEngine fe = null;	// 실제 인스턴스가 없는 null값 참조변수를 형변환 시 멤버 사용 불가능
+		
+		FireEngine fe2 = (FireEngine)c;
+		Car c2 = fe;
+		c2.drive();		// ERROR, NullPointerException
+		fe2.water();	// ERROR, NullPointerException
+		
+		Car c3 = new Car();	// water()가 없는 멤버 4개짜리 인스턴스
+		
+		FireEngine fe3 = (FireEngine)c3;	// water()를 포함한 멤버 5개짜리 클래스로 형변환 해도
+											// 하지만 실제 인스턴스에 water()가 없어 호출 불가능
+		fe3.water();	// ERROR, ClassCastException
+	}
+}
