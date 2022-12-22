0f235df2968e114ca44cdf444928891944329917
ad9570 <loufs_@naver.com>
2022-12-22 20:59:58 +0700

    userdefined, rethrow, chained exception

--- ch08/src/Ex11UserDefinedException.java
+++ ch08/src/Ex11UserDefinedException.java
@@ -1,8 +1,9 @@
-class Ex8_11 {
+class Ex11UserDefinedException {
 	public static void main(String args[]) {
 		try {
 			startInstall();		// 프로그램 설치에 필요한 준비를 한다.
-			copyFiles();		// 파일들을 복사한다. 
+			copyFiles();		// 파일들을 복사한다.
+			System.out.println("설치가 완료되었습니다.");
 		} catch (SpaceException e)	{
 			System.out.println("에러 메시지 : " + e.getMessage());
 			e.printStackTrace();
@@ -10,33 +11,37 @@ class Ex8_11 {
 		} catch (MemoryException me)	{
 			System.out.println("에러 메시지 : " + me.getMessage());
 			me.printStackTrace();
-			System.gc();         // Garbage Collection을 수행하여 메모리를 늘려준다.
-			System.out.println("다시 설치를 시도하세요.");
+			System.out.println("메모리를 확보한 후에 다시 설치하시기 바랍니다.");
 		} finally {
 			deleteTempFiles();		// 프로그램 설치에 사용된 임시파일들을 삭제한다.
-		} // try의 끝
-	} // main의 끝
+		}
+	}
 
    static void startInstall() throws SpaceException, MemoryException { 
 		if(!enoughSpace()) 		// 충분한 설치 공간이 없으면...
-			throw new SpaceException("설치할 공간이 부족합니다.");
+			throw new SpaceException("공간이 부족합니다.");
 		if (!enoughMemory())		// 충분한 메모리가 없으면...
 			throw new MemoryException("메모리가 부족합니다.");
-   } // startInstall메서드의 끝
+	   System.out.println("설치를 시작합니다.");
+   }
 
-   static void copyFiles() { /* 파일들을 복사하는 코드를 적는다. */ }
-   static void deleteTempFiles() { /* 임시파일들을 삭제하는 코드를 적는다.*/ }
+   static void copyFiles() {
+	   System.out.println("파일을 복사합니다.");
+   }
+   static void deleteTempFiles() {
+	   System.out.println("임시 파일을 삭제합니다.");
+   }
    
    static boolean enoughSpace()   {
-		// 설치하는데 필요한 공간이 있는지 확인하는 코드를 적는다.
+	   System.out.println("설치에 필요한 공간이 있는지 확인합니다.");
 		return false;
    }
    
 	static boolean enoughMemory() {
-		// 설치하는데 필요한 메모리공간이 있는지 확인하는 코드를 적는다.
-		return true;
+		System.out.println("설치에 필요한 메모리가 있는지 확인합니다.");
+		return false;
    }
-} // ExceptionTest클래스의 끝
+}
 
 class SpaceException extends Exception {
 	SpaceException(String msg) {
