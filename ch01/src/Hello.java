76c70ef803bea2445f0168658bb8e74e548d710e
MinWooPark <narris64@gmail.com>
2023-12-29 00:29:48 +0900

    dir2

--- ch01/src/Hello.java
+++ ch01/src/Hello.java
@@ -1,14 +1,18 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 public class Hello {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         System.out.println("Hello, world.");
         File f = new File("C:\\Users\\PMW\\Desktop\\fwef.txt");
         System.out.println("f = " + f);
-        FileInputStream fi = new FileInputStream("C:\\Users\\PMW\\Desktop\\fwef.txt");
-        FileOutputStream fo = new FileOutputStream("C:\\Users\\PMW\\Desktop\\fwet.txt");
+        try (FileInputStream fi = new FileInputStream("C:\\Users\\PMW\\Desktop\\fwef.txt");
+             FileOutputStream fo = new FileOutputStream("C:\\Users\\PMW\\Desktop\\fwet.txt")) {
+            System.out.println(fi);
+            System.out.println(fo);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
